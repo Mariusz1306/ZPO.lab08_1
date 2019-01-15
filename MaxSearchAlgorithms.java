976ee653d6aeef43ed1ba7e38f00fd70bf356c0d
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2019-01-15 13:01:49 +0100

    Update MaxSearchAlgorithms.java

--- MaxSearchAlgorithms.java
+++ MaxSearchAlgorithms.java
@@ -2,61 +2,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MaxSearchAlgorithms {
-    private int Max;
+    private int Max; //Maksymalny znaleziony element
 
-    public List leftScan(List<Integer> lista){
+    //Według specyfikacji wszystkie metody skanujące mają zwracać listę elementów, które były „chwilowymi” elementami maksymalnymi
+    
+    public List leftScan(List<Integer> lista){ //Skanowanie od lewej
         ArrayList result = new ArrayList();
-        int max = lista.get(0);
-        result.add(max);
-        for (int i = 1; i < lista.size(); i++){
-            if (lista.get(i) > max) {
-                max = lista.get(i);
-                result.add(max);
+        int max = lista.get(0); //Przyjmij, że największa liczba jest pierwsza
+        result.add(max); //dodajemy ją do wyniku (listy "chwilowych" elementów maksymalnych)
+        for (int i = 1; i < lista.size(); i++){ //Przejdź przez wszystkie elementy, poza pierwszym, który już obsłużyliśmy
+            if (lista.get(i) > max) { //Jeśli dany element jest większy od naszego "chwilowego" maksimum
+                max = lista.get(i); //Przyjmij, że ten element jest nowym "chwilowym" maksimum
+                result.add(max); //Dodaj go do wyniku
             }
         }
-        setMax(max);
+        setMax(max); //Na sam koniec ustaw prawdziwe maksimum jako Max
         return result;
     }
 
-    public List rightScan(List<Integer> lista){
+    public List rightScan(List<Integer> lista){ //Skanowanie od prawej
         ArrayList result = new ArrayList();
-        int max = lista.get(lista.size()-1);
-        result.add(max);
-        for (int i = lista.size()-1; i > 0; i--) {
-            if (lista.get(i) > max) {
-                max = lista.get(i);
-                result.add(max);
+        int max = lista.get(lista.size()-1); //Przyjmij, że największa liczba jest ostatnia
+        result.add(max); //dodaj do wyniku
+        for (int i = lista.size()-2; i > 0; i--) { //Przejdź przez wszystkie elementy w odwrotnej kolejności, poza ostatnim, który już obsłużyliśmy
+            if (lista.get(i) > max) { //Jeśli dany element jest większy od naszego "chwilowego" maksimum
+                max = lista.get(i); //Przyjmij, że ten element jest nowym "chwilowym" maksimum
+                result.add(max); //Dodaj go do wyniku
             }
         }
-        setMax(max);
+        setMax(max); //Na sam koniec ustaw prawdziwe maksimum jako Max
         return result;
     }
 
-    public List evenOddScan(List<Integer> lista){
+    public List evenOddScan(List<Integer> lista){ //Skanowanie najpierw przystych indeksów listy, a następnie nieparzystych
         ArrayList result = new ArrayList();
-        int max = lista.get(0);
-        result.add(max);
-        for (int i = 2; i < lista.size(); i+=2) {
-            if (lista.get(i) > max) {
-                max = lista.get(i);
-                result.add(max);
+        int max = lista.get(0); //Przyjmij, że najwięksa liczba jest pierwsza, ponieważ przyjmujemy, że indeks 0 jest indeksem parzystym
+        result.add(max); //dodaj do wyniku
+        for (int i = 2; i < lista.size(); i+=2) { //Przejdź przez wszystkie elementy na parzystych indeksach, poza pierwszym, który już obsłużyliśmy
+            if (lista.get(i) > max) { //Jeśli dany element jest większy od naszego "chwilowego" maksimum
+                max = lista.get(i); //Przyjmij, że ten element jest nowym "chwilowym" maksimum
+                result.add(max); //Dodaj go do wyniku
             }
         }
-        for (int i = 1; i < lista.size(); i+=2) {
-            if (lista.get(i) > max) {
-                max = lista.get(i);
-                result.add(max);
+        for (int i = 1; i < lista.size(); i+=2) { //Przejdź przez wszystkie elementy na nieparzystych indeksach
+            if (lista.get(i) > max) { //Jeśli dany element jest większy od naszego "chwilowego" maksimum
+                max = lista.get(i); //Przyjmij, że ten element jest nowym "chwilowym" maksimum
+                result.add(max); //Dodaj go do wyniku
             }
         }
-        setMax(max);
+        setMax(max); //Na sam koniec ustaw prawdziwe maksimum jako Max
         return result;
     }
 
-    public int getMax() {
+    //Pomocnicze metody:
+    
+    public int getMax() { //Zwraca ostatnie znalezione maksimum (nie używana oficjalnie)
         return Max;
     }
 
-    public void setMax(int max) {
+    public void setMax(int max) { //Ustawia maksimum
         Max = max;
     }
 }
